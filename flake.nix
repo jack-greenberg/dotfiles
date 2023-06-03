04c0b484cd8619a779ea80e314de47f3b32f2d92
Jack Greenberg <j@jackgreenberg.co>
2023-06-02 22:57:02 -0600

    Add initial dotfiles

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "My Home Manager flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      formatter.${system} = pkgs.nixpkgs-fmt;
+      homeConfigurations.jack = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [
+          ./home.nix
+        ];
+      };
+
+    };
+}
---
+++ home.nix
@@ -0,0 +1,88 @@
+{ config, pkgs, ... }:
+
+{
+  home = {
+    username = "jack";
+    homeDirectory = "/home/jack";
+
+    stateVersion = "22.11";
+
+    packages = with pkgs; [
+      ripgrep
+      fzf
+      jq
+      tree
+      tmux
+      bazelisk
+      meld
+      starship
+    ];
+
+    file = {
+      ".zshrc".source = dotfiles/zshrc;
+      ".config/starship.toml".source = dotfiles/starship.toml;
+    };
+
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+  };
+
+  programs = {
+    bat = {
+      enable = true;
+      config = {
+        theme = "Github";
+        italic-text = "always";
+      };
+    };
+
+    gh = {
+      enable = true;
+      settings = {
+        aliases = {
+          web = "repo view --web";
+        };
+        git_protocol = "ssh";
+        pager = "cat";
+        editor = "nvim";
+      };
+    };
+
+    git = {
+      enable = true;
+      userName = "Jack Greenberg";
+      userEmail = "j@jackgreenberg.co";
+      aliases = {
+        lg = "log --decorate --graph --oneline";
+        lag = "log --branches --decorate --graph --oneline";
+      };
+      extraConfig = {
+        core = {
+          editor = "/usr/bin/nvim";
+          pager = "less -F -X";
+        };
+        pager = {
+          branch = false;
+          grep = false;
+          stash = false;
+          log = true;
+        };
+        init = {
+          defaultBranch = "main";
+        };
+        merge = {
+          tool = "meld";
+        };
+      };
+    };
+
+    zsh = {
+      oh-my-zsh = {
+        enable = true;
+      };
+    };
+
+    home-manager.enable = true;
+  };
+}
